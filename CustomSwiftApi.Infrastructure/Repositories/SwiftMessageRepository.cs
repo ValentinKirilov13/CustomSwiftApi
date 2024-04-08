1a7bcd6c371420b187de6198492c2476a92adc22
Valentin Kirilov <valkilmaar@gmail.com>
2024-04-08 23:40:38 +0300

    Initial add SwiftMessageRepository

---
+++ CustomSwiftApi.Infrastructure/Repositories/SwiftMessageRepository.cs
@@ -0,0 +1,45 @@
+﻿using CustomSwiftApi.Infrastructure.Contracts;
+using CustomSwiftApi.Infrastructure.Models;
+using Microsoft.Data.Sqlite;
+
+namespace CustomSwiftApi.Infrastructure.Repositories
+{
+    public class SwiftMessageRepository : IRepository<SwiftMessage>
+    {
+        private readonly SqliteConnection _connection;
+
+        public SwiftMessageRepository(SqliteConnection connection)
+        {
+            _connection = connection;
+            InitializeDatabaseAsync().GetAwaiter().GetResult();
+        }
+
+        public async Task Insert(SwiftMessage entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task InitializeDatabaseAsync()
+        {
+            using (_connection)
+            {
+                try
+                {
+                    await _connection.OpenAsync();
+
+                    string sqlQuery = "";
+
+                    using (SqliteCommand command = new SqliteCommand(sqlQuery, _connection))
+                    {
+                        await command.ExecuteNonQueryAsync();
+                    }
+
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+        }
+    }
+}
