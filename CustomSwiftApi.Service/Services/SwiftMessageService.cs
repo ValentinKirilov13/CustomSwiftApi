060d3f16814cb9a6a0f6db6723129c26d17821b5
Valentin Kirilov <valkilmaar@gmail.com>
2024-04-08 23:42:30 +0300

    Initial add SwiftMessageService

---
+++ CustomSwiftApi.Service/Services/SwiftMessageService.cs
@@ -0,0 +1,18 @@
+﻿using CustomSwiftApi.Infrastructure.Contracts;
+using CustomSwiftApi.Infrastructure.Models;
+using CustomSwiftApi.Service.Contracts;
+
+namespace CustomSwiftApi.Service.Services
+{
+    public class SwiftMessageService : ISwiftMessageService
+    {
+        private readonly IRepository<SwiftMessage> _repository;
+
+        public SwiftMessageService(IRepository<SwiftMessage> repository)
+        {
+            _repository = repository;
+        }
+
+
+    }
+}
