f18de512005798829e60aee174b80a5414ccff40
Valentin Kirilov <valkilmaar@gmail.com>
2024-04-08 23:43:52 +0300

    Initial add SwiftMessageController

---
+++ CustomSwiftApi.Api/Controllers/SwiftMessageController.cs
@@ -0,0 +1,18 @@
+﻿using CustomSwiftApi.Service.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomSwiftApi.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SwiftMessageController : ControllerBase
+    {
+        private readonly ISwiftMessageService _swiftMessageService;
+
+        public SwiftMessageController(ISwiftMessageService swiftMessageService)
+        {
+            _swiftMessageService = swiftMessageService;
+        }
+     
+    }
+}
