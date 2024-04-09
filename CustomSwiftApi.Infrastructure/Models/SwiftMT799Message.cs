b9b1bf68cb5601496b758473a81be250e4be1a8f
Valentin Kirilov <valkilmaar@gmail.com>
2024-04-09 21:39:42 +0300

    Add SwiftMt799Message db entity

--- CustomSwiftApi.Infrastructure/Models/SwiftMT799Message.cs
+++ CustomSwiftApi.Infrastructure/Models/SwiftMT799Message.cs
@@ -1,7 +1,43 @@
 ﻿namespace CustomSwiftApi.Infrastructure.Models
 {
+    /// <summary>
+    ///  Swift MT799 message database model
+    /// </summary>
     public class SwiftMT799Message
     {
+        /// <summary>
+        ///  Field 1: Basic Header Block
+        /// </summary>
+        public required string BasicHeaderBlock { get; set; }
 
+        /// <summary>
+        ///  Field 2: Application Header Block
+        /// </summary>
+        public required string ApplicationHeaderBlock { get; set; }
+
+        /// <summary>
+        ///  Field 20: Transaction Reference Number
+        /// </summary>
+        public required string TransactionReferenceNumber { get; set; }
+
+        /// <summary>
+        ///  Field 21: Related Reference
+        /// </summary>
+        public string? RelatedReference { get; set; }
+
+        /// <summary>
+        ///  Field 79: Narrative
+        /// </summary>
+        public required string Narrative { get; set; }
+
+        /// <summary>
+        ///  Field MAC: Message Authentication Code
+        /// </summary>
+        public required string MAC {  get; set; }
+
+        /// <summary>
+        ///  Field CHK: Checksum
+        /// </summary>
+        public required string CHK { get; set; }
     }
 }
