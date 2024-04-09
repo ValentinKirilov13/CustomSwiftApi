82848aa11bd3a40bcf9378109b31b5087cd8dc86
Valentin Kirilov <valkilmaar@gmail.com>
2024-04-09 21:42:05 +0300

    Add functionality to insert data into database and create initial table in it

--- CustomSwiftApi.Infrastructure/Repositories/SwiftMT799MessageRepository.cs
+++ CustomSwiftApi.Infrastructure/Repositories/SwiftMT799MessageRepository.cs
@@ -1,6 +1,9 @@
 ﻿using CustomSwiftApi.Infrastructure.Contracts;
+using CustomSwiftApi.Infrastructure.Exeptions;
 using CustomSwiftApi.Infrastructure.Models;
 using Microsoft.Data.Sqlite;
+using static CustomSwiftApi.Infrastructure.Constants.QueryConstants;
+using static CustomSwiftApi.Infrastructure.ErrorMessages.ErrorMessage;
 
 namespace CustomSwiftApi.Infrastructure.Repositories
 {
@@ -14,22 +17,60 @@ namespace CustomSwiftApi.Infrastructure.Repositories
             InitializeDatabaseAsync().GetAwaiter().GetResult();
         }
 
-        public async Task Insert(SwiftMT799Message entity)
+        /// <summary>
+        /// Establish a link with the database, then input the information from the SwiftMT799Message entity into the database
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        /// <exception cref="InvalidInsertStatementExeption"></exception>
+        public async Task<bool> InsertAsync(SwiftMT799Message entity)
         {
-            throw new NotImplementedException();
-        }
-
-        private async Task InitializeDatabaseAsync()
-        {
-            using (_connection)
+            await using (_connection)
             {
                 try
                 {
                     await _connection.OpenAsync();
 
-                    string sqlQuery = "";
+                    string sqlQuery = InsertDataInSwiftMT799Messages;
 
-                    using (SqliteCommand command = new SqliteCommand(sqlQuery, _connection))
+                    await using (SqliteCommand command = new SqliteCommand(sqlQuery, _connection))
+                    {
+                        command.Parameters.AddWithValue("@BasicHeaderBlock", entity.BasicHeaderBlock);
+                        command.Parameters.AddWithValue("@ApplicationHeaderBlock", entity.ApplicationHeaderBlock);
+                        command.Parameters.AddWithValue("@TransactionReferenceNumber", entity.TransactionReferenceNumber);
+                        command.Parameters.AddWithValue("@RelatedReference", entity.RelatedReference);
+                        command.Parameters.AddWithValue("@Narrative", entity.Narrative);
+                        command.Parameters.AddWithValue("@MAC", entity.MAC);
+                        command.Parameters.AddWithValue("@CHK", entity.CHK);
+
+                        int affectRows = await command.ExecuteNonQueryAsync();
+
+                        return affectRows > 0;
+                    }
+                }
+                catch (Exception)
+                {
+                    throw new InvalidInsertStatementExeption(InvalidInsertStatementErrorMessage);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Establish a link with the database, then create a table SwiftMT799Messages if not exist.
+        /// </summary>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        /// <exception cref="InvalidInitializeDatabaseExeption"></exception>
+        private async Task InitializeDatabaseAsync()
+        {
+            await using (_connection)
+            {
+                try
+                {
+                    await _connection.OpenAsync();
+
+                    string sqlQuery = CreateTableSwiftMT799Messages;
+
+                    await using (SqliteCommand command = new SqliteCommand(sqlQuery, _connection))
                     {
                         await command.ExecuteNonQueryAsync();
                     }
@@ -37,7 +78,7 @@ namespace CustomSwiftApi.Infrastructure.Repositories
                 }
                 catch (Exception)
                 {
-
+                    throw new InvalidInitializeDatabaseExeption(InvalidInitializeDatabaseErrorMessage);
                 }
             }
         }
