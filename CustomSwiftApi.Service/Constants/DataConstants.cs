10d967f82b0271741d957eaa4d63b56521579d89
Valentin Kirilov <valkilmaar@gmail.com>
2024-04-09 21:43:02 +0300

    Add data constants

---
+++ CustomSwiftApi.Service/Constants/DataConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomSwiftApi.Service.Constants
+{
+    public static class DataConstants
+    {
+        public const int BasicHeaderBlockMaxLength = 25;
+        public const int ApplicationHeaderBlockMaxLength = 47;
+        public const int TransactionReferenceNumberMaxLength = 16;
+        public const int RelatedReferenceMaxLength = 16;
+        public const int NarrativeMaxLength = 1750;
+        public const int MACMaxLength = 8;
+        public const int CHKMaxLength = 12;
+    }
+}
