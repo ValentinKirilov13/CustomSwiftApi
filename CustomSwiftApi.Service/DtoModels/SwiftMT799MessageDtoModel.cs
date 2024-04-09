b049f46a964ee347e7cb2756e9e86acb968aaf9f
Valentin Kirilov <valkilmaar@gmail.com>
2024-04-09 21:43:49 +0300

    Add data transfer object for SwiftMT799 message

--- CustomSwiftApi.Service/DtoModels/SwiftMT799MessageDtoModel.cs
+++ CustomSwiftApi.Service/DtoModels/SwiftMT799MessageDtoModel.cs
@@ -1,7 +1,60 @@
-﻿namespace CustomSwiftApi.Service.DtoModels
+﻿using System.ComponentModel.DataAnnotations;
+using static CustomSwiftApi.Service.Constants.DataConstants;
+using static CustomSwiftApi.Infrastructure.ErrorMessages.ErrorMessage;
+
+namespace CustomSwiftApi.Service.DtoModels
 {
+    /// <summary>
+    ///  Swift MT799 message DTO model
+    /// </summary>
     public class SwiftMT799MessageDtoModel
     {
+        /// <summary>
+        ///  Field 1: Basic Header Block
+        /// </summary>
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [MaxLength(BasicHeaderBlockMaxLength, ErrorMessage = MaxLengthErrorMessage)]
+        public required string BasicHeaderBlock { get; set; } = string.Empty;
 
+        /// <summary>
+        ///  Field 2: Application Header Block
+        /// </summary>
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [MaxLength(ApplicationHeaderBlockMaxLength, ErrorMessage = MaxLengthErrorMessage)]
+        public required string ApplicationHeaderBlock { get; set; } = string.Empty;
+
+        /// <summary>
+        ///  Field 20: Transaction Reference Number
+        /// </summary>
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [MaxLength(TransactionReferenceNumberMaxLength, ErrorMessage = MaxLengthErrorMessage)]
+        public required string TransactionReferenceNumber { get; set; } = string.Empty;
+
+        /// <summary>
+        ///  Field 21: Related Reference
+        /// </summary>
+        [MaxLength(RelatedReferenceMaxLength, ErrorMessage = MaxLengthErrorMessage)]
+        public string? RelatedReference { get; set; }
+
+        /// <summary>
+        ///  Field 79: Narrative
+        /// </summary>
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [MaxLength(NarrativeMaxLength, ErrorMessage = MaxLengthErrorMessage)]
+        public required string Narrative { get; set; } = string.Empty;
+
+        /// <summary>
+        ///  Field MAC: Message Authentication Code
+        /// </summary>
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [MaxLength(MACMaxLength, ErrorMessage = MaxLengthErrorMessage)]
+        public required string MAC { get; set; } = string.Empty;
+
+        /// <summary>
+        ///  Field CHK: Checksum
+        /// </summary>
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [MaxLength(CHKMaxLength, ErrorMessage = MaxLengthErrorMessage)]
+        public required string CHK { get; set; } = string.Empty;
     }
 }
