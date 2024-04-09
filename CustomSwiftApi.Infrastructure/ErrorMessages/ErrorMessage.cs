126df6586fe8bcdf96ed94de124b43d6088fa749
Valentin Kirilov <valkilmaar@gmail.com>
2024-04-09 21:38:58 +0300

    Add error messages

---
+++ CustomSwiftApi.Infrastructure/ErrorMessages/ErrorMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace CustomSwiftApi.Infrastructure.ErrorMessages
+{
+    public static class ErrorMessage
+    {
+        public const string RequiredErrorMessage = "The field {0} is required!";
+        public const string MaxLengthErrorMessage = "The field {0} must be with max {1} charachters length!";
+
+        public const string InvalidInsertStatementErrorMessage = "Invalid insert opertaion in database";
+        public const string InvalidInitializeDatabaseErrorMessage = "Invalid initialize database";        
+    }
+}
